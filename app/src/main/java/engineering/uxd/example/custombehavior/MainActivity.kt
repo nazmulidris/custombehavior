5cc8ffb8dde12032a221204b5e77e05cc7ba2f56
Nazmul Idris <nazmul@fasterlap.com>
2018-04-21 14:54:57 -0700

    Migrate project to Kotlin

---
+++ app/src/main/java/engineering/uxd/example/custombehavior/MainActivity.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 Nazmul Idris. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package engineering.uxd.example.custombehavior
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar.LENGTH_SHORT
+import android.support.design.widget.Snackbar.make
+import android.support.v7.app.AppCompatActivity
+import com.bignerdranch.android.custombehavior.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab_rotate.setOnClickListener {
+            make(container, "Hola - Fab Rotate pressed!", LENGTH_SHORT)
+                    .show()
+        }
+
+        fab_scale.setOnClickListener {
+            make(container, "Hola - Fab Scale pressed!", LENGTH_SHORT)
+                    .show()
+        }
+    }
+
+}
---
+++ app/src/main/java/engineering/uxd/example/custombehavior/RotateBehavior.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2018 Nazmul Idris. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package engineering.uxd.example.custombehavior
+
+import android.content.Context
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.util.AttributeSet
+import android.view.View
+import org.jetbrains.anko.AnkoLogger
+
+class RotateBehavior(context: Context, attrs: AttributeSet) :
+        CoordinatorLayout.Behavior<FloatingActionButton>(context, attrs),
+        AnkoLogger {
+
+    override fun layoutDependsOn(parent: CoordinatorLayout,
+                                 child: FloatingActionButton,
+                                 dependency: View): Boolean {
+        return dependency is Snackbar.SnackbarLayout
+    }
+
+    override fun onDependentViewChanged(parent: CoordinatorLayout,
+                                        child: FloatingActionButton,
+                                        dependency: View): Boolean {
+        val translationY = getFabTranslationYForSnackbar(parent, child)
+        val percentComplete = -translationY / dependency.height
+        child.rotation = -90 * percentComplete
+        child.translationY = translationY
+        return false
+    }
+
+    private fun getFabTranslationYForSnackbar(parent: CoordinatorLayout,
+                                              fab: FloatingActionButton): Float {
+        var minOffset = 0f
+        val dependencies = parent.getDependencies(fab)
+
+        dependencies.forEach {
+
+            if (it is Snackbar.SnackbarLayout && parent.doViewsOverlap(fab, it)) {
+                minOffset = Math.min(minOffset, it.getTranslationY() - it.getHeight())
+            }
+
+        }
+
+        return minOffset
+    }
+
+}
