e675e504138385bdd4972fc4a4423be557526a50
Nazmul Idris <nazmul@fasterlap.com>
2018-04-21 20:05:19 -0700

    Fix package name

--- app/src/main/java/engineering/uxd/example/custombehavior/MainActivity.kt
+++ app/src/main/java/engineering/uxd/example/custombehavior/MainActivity.kt
@@ -20,7 +20,6 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar.LENGTH_SHORT
 import android.support.design.widget.Snackbar.make
 import android.support.v7.app.AppCompatActivity
-import com.bignerdranch.android.custombehavior.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
